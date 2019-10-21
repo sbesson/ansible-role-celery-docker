38f95e8f642c63901ad4504ead126a9f6cbfaeec
Sebastien Besson <seb.besson@gmail.com>
2019-10-21 22:15:45 +0100

    Upgrade role tests to Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,28 +1,18 @@
-import testinfra.utils.ansible_runner
+import os
 import pytest
-from time import sleep
+import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
-
-
-@pytest.mark.parametrize("name", [
-    "celery-worker",
-    "docker",
-    "redis",
-])
-def test_services_running_and_enabled(Service, name):
-    assert Service(name).is_running
-    assert Service(name).is_enabled
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 @pytest.mark.skip(reason="needs to be reviewed")
-def test_celery_task(Command, File, Sudo):
-    with Sudo():
-        Command.check_output(
+def test_celery_task(host):
+    with host.sudo():
+        host.check_output(
             'rm -f /tmp/celery/output.txt /tmp/celery/log.out')
 
-    taskid = Command.check_output(
+    taskid = host.check_output(
         '/opt/celery/venv/bin/python %s %s %s %s %s %s %s %s %s %s %s %s',
         '/opt/celery/worker/tasks.py',
         '--inputpath', '/tmp/celery/input.txt',
@@ -37,18 +27,18 @@ def test_celery_task(Command, File, Sudo):
     # WARNING: If the network is particularly slow the download of busybox
     # may take too long
     for i in xrange(20):
-        got_outputs = (File('/tmp/celery/log.out').exists and
-                       File('/tmp/celery/output.txt').exists)
+        got_outputs = (host.file('/tmp/celery/log.out').exists and
+                       host.file('/tmp/celery/output.txt').exists)
         # Sleep first to allow time for writing
         sleep(10)
         if got_outputs:
             break
 
-    flog = File('/tmp/celery/log.out')
+    flog = host.file('/tmp/celery/log.out')
     assert flog.exists
     assert flog.content_string.strip() == 'Done'
 
-    fout = File('/tmp/celery/output.txt')
+    fout = host.file('/tmp/celery/output.txt')
     assert fout.exists
     assert fout.content_string.strip() == \
         '5eb63bbbe01eeed093cb22bb8f5acdc3  /input'
